4a8dff7bdca295e2013a873ca09882c4f7557ff1
ZungTa <quddnr0708@gmail.com>
2021-05-04 14:56:46 +0900

    store sample

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,23 @@
+import { useRootDispatch, useRootState } from '@/store/RootStore';
 import { css } from '@emotion/react';
 
 function Footer() {
+  const rootState = useRootState();
+  const rootDispatch = useRootDispatch();
+
+  function setProjectName() {
+    rootDispatch({
+      type: 'setProjectName',
+      newProjectName: 'Boiler Plate',
+    });
+  }
+
   return (
     <div css={footerCss}>
       <div className="footer-container">
-        <h1>Footer</h1>
+        <h1>Footer, {rootState.projectName}</h1>
+
+        <button onClick={setProjectName}>setProjectName</button>
       </div>
     </div>
   );
