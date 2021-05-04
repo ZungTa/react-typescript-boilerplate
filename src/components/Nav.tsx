7b52a10fa97580d1fb58c842260a88fb32b8a646
ZungTa <quddnr0708@gmail.com>
2021-05-04 14:50:28 +0900

    i18n sample

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,13 @@
 import { css } from '@emotion/react';
+import { useTranslation } from 'react-i18next';
 
 function Nav() {
+  const { t } = useTranslation();
+
   return (
     <div css={navCss}>
       <div className="nav-container">
-        <h1>Nav</h1>
+        <h1>Nav, {t('name')}</h1>
       </div>
     </div>
   );
